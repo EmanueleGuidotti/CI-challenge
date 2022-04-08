5e0e3aab388d5d462222f11b0519f984977b3b4a
emanuele.guidotti <guidotti.ema@gmail.com>
2022-04-08 17:59:35 +0200

    added comment to movies Api

--- src/services/movies.services.js
+++ src/services/movies.services.js
@@ -1,6 +1,10 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { axiosBaseQuery } from "../settings/axiosInstance";
 
+/**
+ * Movies Api
+ * @type {Api<BaseQueryFn, {getPopularMovies: *, getMovieById: *}, string, never, typeof coreModuleName> | Api<BaseQueryFn, {getPopularMovies: *, getMovieById: *}, string, never, any>}
+ */
 export const moviesApi = createApi({
   reducerPath: "movies",
   baseQuery: axiosBaseQuery,
